5a03630bc4abb08502275fa8a87389f66b9e30e2
Konstantin Nesterov <wasd171@gmail.com>
2017-11-10 19:49:56 +0100

    Added styletron

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'sanitize.css'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Styletron from 'styletron-server'
+import { StyletronProvider } from 'styletron-react'
+
+ReactDOM.render(
+	<StyletronProvider styletron={new Styletron()}>
+		<App />
+	</StyletronProvider>,
+	document.getElementById('root')
+)
+registerServiceWorker()
